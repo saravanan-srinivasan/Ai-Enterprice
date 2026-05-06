0dd0ef27203c29bd535f2990c669588d9d87f70a
saravanan-srinivasan <msshravan2004@gmail.com>
2026-05-06 13:00:34 +0530

    fix structlog render issue

--- utils/logger.py
+++ utils/logger.py
@@ -97,7 +97,7 @@ def configure_logging(log_level: str = "INFO", log_format: str = "json") -> None
             getattr(logging, log_level.upper(), logging.INFO)
         ),
         context_class=dict,
-        logger_factory=structlog.PrintLoggerFactory(),
+        logger_factory=structlog.stdlib.LoggerFactory(),
         cache_logger_on_first_use=True,
     )
 
